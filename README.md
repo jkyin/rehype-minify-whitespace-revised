01ce8b3117432d7c6bac826da0cb30feda9cbcd5
jkyin <yinxiaoyu888@gmail.com>
2023-04-02 19:02:14 +0800

    feat: initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# rehype-minify-whitespace-x
+source from rehype-minify-whitespace, but keep whitespace inside elements.
+
+check out https://github.com/rehypejs/rehype-minify/blob/main/packages/rehype-minify-whitespace/readme.md
---
+++ example.js
@@ -0,0 +1,17 @@
+// import {read} from 'to-vfile'
+// import {unified} from 'unified'
+// import rehypeParse from 'rehype-parse'
+// import rehypeStringify from 'rehype-stringify'
+// import rehypeMinifyWhitespace from './index.js'
+
+// main()
+
+// async function main() {
+//   const file = await unified()
+//     .use(rehypeParse)
+//     .use(rehypeMinifyWhitespace)
+//     .use(rehypeStringify)
+//     .process(await read('example.md'))
+
+//   console.log(String(file))
+// }
